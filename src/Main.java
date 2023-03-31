7a168c9f9ace5cdaabf89f5ebec5d7a71c5b8de4
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2023-03-31 22:44:14 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+}
